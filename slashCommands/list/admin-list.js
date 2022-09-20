ba2d805768c565885c833ff6996f2d16318bbd2a
Itz_Toothless <80362356+Itz-Toothless@users.noreply.github.com>
2022-09-20 16:03:29 +0200

    Update admin-list.js

--- slashCommands/list/admin-list.js
+++ slashCommands/list/admin-list.js
@@ -6,11 +6,11 @@ module.exports = {
     description: 'Zeigt alle Admins des Servers und die Rollen, die denen die Administrator Berechtigung geben',
     cooldown: 5000,
     usage: '/admin-list',
-    userPerms: ['Administrator'],
+    userPerms: ['ManageServer'],
     botPerms: [],
     run: async (client, interaction) => {
         try {
-            const list = client.guilds.cache.get('1009195382426832906')
+            const list = await client.guilds.fetch(`${interaction.guild.id}`)
             const members = await list.members.fetch();
             let arr = [];
             members.forEach((member) => {
@@ -19,15 +19,20 @@ module.exports = {
                     arr.push(`**Nutzer:** ${member.user} - **Rollen:** ${rolesAdmin}`);
                 }
             });
+            let loadingEmbed = new EmbedBuilder()
+                .setColor('Random')
+                .setTitle('Admin Liste wird geladen...')
+                .setFooter({ text: 'Programmiert von ' + client.users.cache.get('705557092802625576').tag, iconURL: interaction.user.displayAvatarURL({ dynamic: true }) })
+                .setTimestamp()
             let adminEmbed = new EmbedBuilder()
                 .setColor('Random')
                 .setTitle('Admin Liste')
-                .setDescription(`${arr.join('\n')}`)
+                .setDescription(`${arr.join('\n') ? `${arr.join('\n')}` : 'Es sind keine Admins auf diesem Server!'}`)
                 .setFooter({ text: 'Programmiert von ' + client.users.cache.get('705557092802625576').tag, iconURL: interaction.user.displayAvatarURL({ dynamic: true }) })
                 .setTimestamp()
-            await interaction.deferReply({ ephemeral: true });
-            await wait(500);
-            return await interaction.followUp({ embeds: [adminEmbed], ephemeral: true, allowedMentions: { repliedUser: false } });
+            await interaction.reply({ embeds: [loadingEmbed], ephemeral: false, allowedMentions: { repliedUser: false }  })
+            await wait(1000);
+            return await interaction.editReply({ embeds: [adminEmbed] });
         } catch (err) {
             console.log(err);
         }
