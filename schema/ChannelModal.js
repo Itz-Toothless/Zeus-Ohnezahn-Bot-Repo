0054c7c8fbe0df6b2542586a6cef003e8f02102d
Itz_Toothless <80362356+Itz-Toothless@users.noreply.github.com>
2022-09-22 19:03:38 +0200

    Create ChannelModal.js

---
+++ schema/ChannelModal.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const channelSchema = new mongoose.Schema({
+    GuildID: String,
+    ChannelID: String
+});
+
+module.exports = mongoose.model('channelModal', channelSchema);
