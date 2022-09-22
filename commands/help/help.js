6c8821fe28446a57d74722bb1fc820d8ca60ccb2
Itz_Toothless <80362356+Itz-Toothless@users.noreply.github.com>
2022-09-22 19:08:01 +0200

    Add files via upload

--- commands/help/help.js
+++ commands/help/help.js
@@ -8,11 +8,26 @@ module.exports = {
     cooldown: 3000,
     userPerms: [],
     botPerms: [],
-    run: async(client, message, args) => {
+    run: async (client, message, args) => {
         try {
             switch (args[0]) {
                 case 'commands' || 'cmds':
-                    let commands = client.commands.map(cmd => `${cmd.name} - ${cmd.description ? cmd.description : 'Keine Beschreibung angegeben'}`).join('\n');
+                    let commands = `help - Zeigt Infos über die jetzigen Kommandos
+                    about - Zeigt Informationen über den Bot
+					admin-list - Zeigt alle Admins des Servers und die Rollen, die denen die Administrator Berechtigung geben
+                    ping - Gibt die jetzige Latenz des Bots aus
+                    meme - Holen Sie sich ein zufälliges Meme
+                    ban - Bannt eine Person
+                    unban - Entbannt eine Person
+                    kick - Kickt eine Person
+                    warn - Verwarnt eine Person
+                    deletewarns - Löscht alle Verwarnungen eines Nutzers
+                    removewarn - Löscht eine Verwarnung eines Nutzers
+                    checkwarns - Zeigt die Verwarnungen eines Nutzers an
+                    set-mod-channel - Legt den Moderations-Kanal fest
+                    set-welcome-channel - Legt den Willkommens-Kanal fest
+                    set-leave-channel - Legt den Verlassen-Kanal fest
+					`
                     let commandsEmbed = new EmbedBuilder()
                         .setColor('#0099ff')
                         .setTitle('Kommandos')
@@ -21,21 +36,28 @@ module.exports = {
                         .setTimestamp();
                     return message.channel.send({ embeds: [commandsEmbed] });
                 case 'aliases':
-                    let aliases = [];
-                    client.commands.forEach(cmd => {
-                        if (cmd.aliases) {
-                            for (let i = 0; i < cmd.aliases.length; i++) {
-                                aliases.push(`${cmd.name} - ${cmd.aliases[i]}`);
-                            }
-                        }
-                        else {
-                            aliases.push(`${cmd.name} - Kein Alias gefunden`);
-                        }
-                    });
+                    let aliases = `binary - bin
+                    string - str
+                    help - h
+                    about - stats
+                    ping - p
+                    admins - admin-list
+                    meme - memes
+                    ban - ban-id
+                    kick - kick-id
+                    unban - Kein Alias gefunden
+                    userinfo - user, u-info, uinfo, user-info, user, u-info, uinfo
+                    warn - warn-user
+                    deletewarns - del-warns, rm-all-warns
+                    warnings - checkwarns
+                    set-mod-channel - smc, set-moderation-c
+                    set-welcome-channel - swc, set-welcome-c
+                    set-leave-channel - slc, set-leave-c
+                    `
                     let aliasesEmbed = new EmbedBuilder()
                         .setColor('#0099ff')
                         .setTitle('Kommando-Aliasse')
-                        .setDescription(aliases.join('\n'))
+                        .setDescription(aliases)
                         .setFooter({ text: 'z&o!help command [commandname] für Kommando-Infos' })
                         .setTimestamp();
                     return message.channel.send({ embeds: [aliasesEmbed] });
@@ -59,7 +81,7 @@ module.exports = {
                         .setTitle('Hilfe')
                         .setDescription('Hier sind alle Kommandos')
                         .addFields({ name: 'Kommandos', value: 'z&o!help commands' },
-                                   { name: 'Aliasse', value: 'z&o!help aliases' })
+                            { name: 'Aliasse', value: 'z&o!help aliases' })
                         .setFooter({ text: 'z&o!help command [commandname] für Kommando-Infos' })
                         .setTimestamp();
                     return message.channel.send({ embeds: [helpEmbed] });
@@ -68,4 +90,4 @@ module.exports = {
             console.log(error);
         }
     }
-}
+};
