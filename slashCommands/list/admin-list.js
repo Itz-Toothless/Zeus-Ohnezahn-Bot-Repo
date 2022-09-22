209fc8f17aa171f40693caeadc1a217af15beb7f
Itz_Toothless <80362356+Itz-Toothless@users.noreply.github.com>
2022-09-22 19:01:59 +0200

    Update admin-list.js

--- slashCommands/list/admin-list.js
+++ slashCommands/list/admin-list.js
@@ -6,11 +6,11 @@ module.exports = {
     description: 'Zeigt alle Admins des Servers und die Rollen, die denen die Administrator Berechtigung geben',
     cooldown: 5000,
     usage: '/admin-list',
-    userPerms: ['ManageServer'],
+    userPerms: ['ManageGuild'],
     botPerms: [],
     run: async (client, interaction) => {
         try {
-            const list = await client.guilds.fetch(`${interaction.guild.id}`)
+            const list = client.guilds.cache.get(`${interaction.guild.id}`);
             const members = await list.members.fetch();
             let arr = [];
             members.forEach((member) => {
@@ -30,7 +30,7 @@ module.exports = {
                 .setDescription(`${arr.join('\n') ? `${arr.join('\n')}` : 'Es sind keine Admins auf diesem Server!'}`)
                 .setFooter({ text: 'Programmiert von ' + client.users.cache.get('705557092802625576').tag, iconURL: interaction.user.displayAvatarURL({ dynamic: true }) })
                 .setTimestamp()
-            await interaction.reply({ embeds: [loadingEmbed], ephemeral: false, allowedMentions: { repliedUser: false }  })
+            await interaction.reply({ embeds: [loadingEmbed], ephemeral: false, allowedMentions: { repliedUser: false } })
             await wait(1000);
             return await interaction.editReply({ embeds: [adminEmbed] });
         } catch (err) {
