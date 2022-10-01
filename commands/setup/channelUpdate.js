68d26e980570f8bdaf682062daada987a704e389
Itz_Toothless <80362356+Itz-Toothless@users.noreply.github.com>
2022-10-01 15:04:15 +0200

    Update channelUpdate.js

--- commands/setup/channelUpdate.js
+++ commands/setup/channelUpdate.js
@@ -1,5 +1,4 @@
 const channelModal = require('../../schema/ChannelModal');
-const blacklistModal = require('../../schema/BlackListModal');
 
 module.exports = {
     name: 'set-channel-channel',
@@ -10,17 +9,6 @@ module.exports = {
     botPerms: [],
     run: async (client, message, args) => {
         try {
-            blacklistModal.findOne({ UserID: message.author.id }, async (err, data) => {
-                if (err) throw err;
-                if (data) {
-                    let blackListEmbed = new EmbedBuilder()
-                        .setColor('Red')
-                        .setTitle('Keine Berechtigung!')
-                        .setDescription(`Sie dürfen diesen Bot nicht nutzen!\nAusgesetzt seit: <t:${data.BlackListTimestamp}:F>`)
-                        .setFooter({ text: `Programmiert von ${client.users.cache.get('705557092802625576').tag} `, iconURL: `${client.users.cache.get('705557092802625576').avatarURL({ dynamic: true })} ` })
-                        .setTimestamp()
-                    return await message.reply({ embeds: [blackListEmbed] });
-                }
                 let channel = message.mentions.channels.first() || message.guild.channels.cache.get(args[0]);
                 if (!channel) {
                     let noChannelEmbed = new EmbedBuilder()
@@ -40,7 +28,7 @@ module.exports = {
                         ChannelID: channel.id,
                     });
                     channelData.save();
-                    message.channel.send('Der neue Nutzer-Update-Log Kanal ist nun <#' + channel.id + '>');
+                    message.channel.send('Der neue Kanal-Update-Log findet nun in ' + channel + ' statt');
                 } else {
                     await channelModal.deleteOne({
                         GuildID: message.guild.id,
@@ -50,7 +38,7 @@ module.exports = {
                         ChannelID: channel.id
                     });
                     newData.save();
-                    message.channel.send('Der neue Nutzer-Update-Log Kanal ist nun <#' + channel.id + '>');
+                    message.channel.send('Der neue Kanal-Update-Log findet nun in ' + channel + ' statt');
                 }
             });
 
