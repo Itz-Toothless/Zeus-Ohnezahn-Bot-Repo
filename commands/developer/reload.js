879b60e1733e0f486f7368fa73bc984b104e05b0
Itz_Toothless <80362356+Itz-Toothless@users.noreply.github.com>
2022-11-06 12:45:20 +0100

    Update reload.js

--- commands/developer/reload.js
+++ commands/developer/reload.js
@@ -10,11 +10,14 @@ module.exports = {
         if (!message.author.id === '705557092802625576') {
             return message.reply({ content: 'Dieses Kommando ist nur für den Entwickler ' + client.users.cache.get('705557092802625576').tag + ' bestimmt!', allowedMentions: { repliedUser: false  }})
         }
+        if (!args[0]) {
+            return message.reply({ content: 'Bitte gebe ein Kommando an' });
+        }
         const commandName = args[0].toLowerCase();
         const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
         if (!command) {
-            return message.channel.send(`There is no command with name or alias \`${commandName ? commandName : 'None'}\`, ${message.author}!`);
+            return message.reply('Bitte gebe ein Kommando an, welcher existent ist!');
         }
 
         const commandFolders = fs.readdirSync('./commands');
@@ -25,7 +28,7 @@ module.exports = {
         try {
             const newCommand = require(`../${folderName}/${command.name}.js`);
             client.commands.set(newCommand.name, newCommand);
-            return message.channel.send(`Das Kommando \`${newCommand.name}\` wurde neu geladen!`);
+            return message.reply({ content: `Das Kommando \`${newCommand.name}\` wurde neu geladen!`, allowedMentions: { repliedUser: false  } });
         } catch (error) {
             console.error(error);
             return message.channel.send(`Ein Fehler ist beim Versuch, \`${command.name}\` neu zu laden, aufgetreten:\n\`${error.message}\``);
