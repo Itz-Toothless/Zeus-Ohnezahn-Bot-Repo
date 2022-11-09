898e385643fff7b2e794aa8ae7826b81e16b47ac
Itz_Toothless <80362356+Itz-Toothless@users.noreply.github.com>
2022-11-09 13:00:31 +0100

    Update about.js

--- slashCommands/info/about.js
+++ slashCommands/info/about.js
@@ -1,47 +1,101 @@
-const { EmbedBuilder } = require("discord.js");
+'use strict';
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 const process = require("process");
 const { loadavg } = require('os');
-const wait = require('node:timers/promises').setTimeout;
+const blacklistModal = require('../../schema/BlackListModal');
 
 module.exports = {
     name: 'about',
     description: "Zeigt Informationen über den Bot",
-    usage: 'w&p!about',
+    usage: '/about',
     cooldown: 3000,
     userPerms: [],
     botPerms: [],
     run: async (client, interaction) => {
         try {
-            let uptime = Math.round(parseInt(client.readyTimestamp) / 1000) - 54;
-            let created = Math.round(parseInt(client.user.createdTimestamp) / 1000);
-            let up = `${process.platform.charAt(0).toUpperCase() + process.platform.slice(1)}`
-            let embed = new EmbedBuilder()
-                .setColor("Blue")
-                .setTimestamp()
-                .setThumbnail(client.user.displayAvatarURL({ dynamic: true }))
-                .setTitle(client.user.username)
-                .setFooter({ text: 'Programmiert von ' + client.users.cache.get('705557092802625576').tag, iconURL: interaction.user.displayAvatarURL({ dynamic: true }) })
-                .setDescription(`
-		> **Entwickler:** <@705557092802625576>
+            blacklistModal.findOne({ UserID: interaction.user.id }, async (err, data) => {
+                if (err) throw err;
+                if (data) {
+                    let blackListEmbed = new EmbedBuilder()
+                        .setColor('Red')
+                        .setTitle('Keine Berechtigung!')
+                        .setDescription(`Sie dürfen diesen Bot nicht nutzen!\nAusgesetzt seit: <t:${data.BlackListTimestamp}:F>`)
+                        .setFooter({ text: `Programmiert von ${client.users.cache.get('705557092802625576').tag} `, iconURL: `${client.users.cache.get('705557092802625576').avatarURL({ dynamic: true })} ` })
+                        .setTimestamp()
+                    return await interaction.reply({ embeds: [blackListEmbed] });
+                }
+                try {
+                    let uptime = Math.round(parseInt(client.readyTimestamp) / 1000) - 60
+                    let created = Math.round(parseInt(client.user.createdTimestamp) / 1000);
+                    let up = `${process.platform.charAt(0).toUpperCase() + process.platform.slice(1)}`
+                    let button = new ActionRowBuilder().addComponents(
+                        new ButtonBuilder()
+                            .setStyle(ButtonStyle.Success)
+                            .setLabel("Aktualisieren")
+                            .setCustomId("rel"))
+                    let embed = new EmbedBuilder()
+                        .setColor('Random')
+                        .setTimestamp()
+                        .setThumbnail(client.user.displayAvatarURL({ dynamic: true }))
+                        .setTitle(client.user.username)
+                        .setFooter({ text: 'Programmiert von ' + client.users.cache.get('705557092802625576').tag, iconURL: interaction.user.displayAvatarURL({ dynamic: true }) })
+                        .setDescription(`
                 > **Server: \`${client.guilds.cache.size}\`**
                 > **Nutzer: \`${Math.ceil(client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString("tr-TR"))}\`**
                 > **Kanäle: \`${client.channels.cache.size}\`**
-                > **Nachrichten Kommandos: \`${client.commands.size - 1}\`**
+                > **Nachrichten Kommandos: \`${client.commands.size - 7}\`**
                 > **Slash Kommandos: \`${client.slashCommands.size}\`**
                 > **Onlinezeit: <t:${uptime}:R>**
                 > **Erstellt: <t:${created}:F>**
                 > **Node.js Version: \`${process.version}\`**
                 > **Discord.js: \`v${require('discord.js').version}\`**
-                > **Betriebssystem des Servers: \`${up}\`**
+                > **Betriebssystem: \`${up}\`**
                 > **CPU: \`${loadavg()[0]}%\`**
                 > **RAM: \`${Math.round(process.memoryUsage().heapUsed / 1024 / 1024 * 100) / 100} MB\`**
-                Lade mich mit [**diesem**](https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot%20applications.commands&permissions=1084516334400) Link auf deinem Server ein
             `)
-	    await interaction.deferReply({ ephemeral: true, fetchReply: true });
-            await wait(500);
-            await interaction.followUp({ embeds: [embed], ephemeral: true });
+                        .addFields({ name: "Einladungs-Link", value: `**[Fügt mich eurem Server hinzu](https://discord.com/api/oauth2/userize?client_id=1017497741871554591&permissions=1503738047638&scope=bot%20applications.commands)**` })
+                    return await interaction.reply({ embeds: [embed], components: [button], ephemeral: true }).then(async () => {
+                        const filter = i => i.user.id === interaction.user.id;
+                        const col = interaction.channel.createMessageComponentCollector({ filter, time: 15000 });
+                        col.on('collect', async (button) => {
+                            if (button.member.id !== interaction.member.id) return;
+                            if(button.customId === 'rel') {
+                                    let embedd = new EmbedBuilder()
+                                        .setColor("Random")
+                                        .setTimestamp()
+                                        .setThumbnail(client.user.displayAvatarURL({ dynamic: true }))
+                                        .setTitle(client.user.username)
+                                        .setFooter({ text: 'Programmiert von ' + client.users.cache.get('705557092802625576').tag, iconURL: interaction.user.displayAvatarURL({ dynamic: true }) })
+                                        .setDescription(`
+                            > **Server: \`${client.guilds.cache.size}\`**
+                            > **Nutzer: \`${Math.ceil(client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString("tr-TR"))}\`**
+                            > **Kanäle: \`${client.channels.cache.size}\`**
+                            > **Nachrichten Kommandos: \`${client.commands.size - 1}\`**
+                            > **Slash Kommandos: \`${client.slashCommands.size}\`**
+                            > **Onlinezeit: <t:${uptime}:R>**
+                            > **Erstellt: <t:${created}:F>**
+                            > **Node.js Version: \`${process.version}\`**
+                            > **Discord.js: \`v${require('discord.js').version}\`**
+                            > **Betriebssystem: \`${up}\`**
+                            > **CPU: \`${loadavg()[0]}%\`**
+                            > **RAM: \`${Math.round(process.memoryUsage().heapUsed / 1024 / 1024 * 100) / 100} MB\`**
+                        `)
+                                        .addFields({ name: "Einladungs-Link", value: `**[Fügt mich eurem Server hinzu](https://discord.com/api/oauth2/userize?client_id=1017497741871554591&permissions=1503738047638&scope=bot%20applications.commands)**` })
+                                    await button.update({ embeds: [embedd], fetchReply: true });
+                            }
+                        });
+                        col.on('end', async() => {
+                            return;
+                        });
+                    }).catch((err) => {
+                        console.log(err);
+                    });
+                } catch (err) {
+                    console.log(err);
+                }
+            });
         } catch (e) {
             console.log(e)
         }
-    }
+    },
 };
